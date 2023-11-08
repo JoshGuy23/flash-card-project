8bca60f476364acd7adba0ca1d45cf7963f29463
Joshua Hecker <jhecker2001@gmail.com>
2023-11-08 10:11:32 -0600

    Added functionality to remove words the user guessed correctly, and save the words to a csv file after every guess.

--- main.py
+++ main.py
@@ -7,8 +7,21 @@ FONT = "Arial"
 LANGUAGE_FILE = "japanese_words_hiragana.csv"
 LEARNING_LANGUAGE = "Hiragana"
 NATIVE_LANGUAGE = "English"
-MAX_INDEX = 17
 current_card = {}
+
+# --------------------------------- SAVE DATA ------------------------------ #
+
+
+def save_data():
+    new_file = pandas.DataFrame(word_dictionary)
+    new_file.to_csv(f"data/{LEARNING_LANGUAGE}_words_to_learn.csv", index=False)
+    generate_word()
+
+
+def remove_correct():
+    word_dictionary.remove(current_card)
+    save_data()
+
 
 # --------------------------------- FLIP CARDS ----------------------------- #
 
@@ -22,7 +35,10 @@ def flip_card():
 
 
 # _________________________________ CREATE FLASH CARDS --------------------- #
-word_file = pandas.read_csv(f"data/{LANGUAGE_FILE}")
+try:
+    word_file = pandas.read_csv(f"data/{LEARNING_LANGUAGE}_words_to_learn.csv")
+except FileNotFoundError:
+    word_file = pandas.read_csv(f"data/{LANGUAGE_FILE}")
 word_dictionary = word_file.to_dict(orient="records")
 
 
@@ -52,18 +68,18 @@ front_card_image = PhotoImage(file="images/card_front.png")
 back_card_image = PhotoImage(file="images/card_back.png")
 card_canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas_image = card_canvas.create_image(400, 263, image=front_card_image)
-lang_text = card_canvas.create_text(400, 150, text="Language", fill="black", font=(FONT, 40, "italic"))
-word_text = card_canvas.create_text(400, 263, text="Word", fill="black", font=(FONT, 50, "bold"))
+lang_text = card_canvas.create_text(400, 150, text="Language", fill="black", font=(FONT, 30, "italic"))
+word_text = card_canvas.create_text(400, 263, text="Word", fill="black", font=(FONT, 40, "bold"))
 card_canvas.grid(row=0, column=0, columnspan=2)
 
 # Right Button
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0, command=generate_word)
+right_button = Button(image=right_image, highlightthickness=0, command=remove_correct)
 right_button.grid(row=1, column=1)
 
 # Wrong Button
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0, command=generate_word)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=save_data)
 wrong_button.grid(row=1, column=0)
 
 timer = window.after(3000, flip_card)
