701d30e40c8e5c2a751fbba4702df96d59fa716b
Joshua Hecker <jhecker2001@gmail.com>
2023-11-08 09:04:16 -0600

    Added functionality to randomly generate words from the language word file, and added functionality to change words using the buttons.

--- main.py
+++ main.py
@@ -8,13 +8,22 @@ LANGUAGE_FILE = "japanese_words_hiragana.csv"
 LEARNING_LANGUAGE = "Hiragana"
 NATIVE_LANGUAGE = "English"
 MAX_INDEX = 17
+
 # _________________________________ CREATE FLASH CARDS --------------------- #
-# TODO: 1. Read word data in.
-# TODO: 2. Pick random word to display on card.
-# TODO: 3. Pressing either button should generate a new random word.
 word_file = pandas.read_csv(f"data/{LANGUAGE_FILE}")
 word_dictionary = word_file.to_dict()
-# word_dictionary[LEARNING_LANGUAGE]
+
+
+def generate_word():
+    language = LEARNING_LANGUAGE
+    word_choice = random.randint(0, len(word_dictionary[LEARNING_LANGUAGE]) - 1)
+    language_word = word_dictionary[LEARNING_LANGUAGE][word_choice]
+
+    card_canvas.itemconfig(lang_text, text=f"{language}")
+    card_canvas.itemconfig(word_text, text=f"{language_word}")
+    pass
+
+
 # --------------------------------- UI SETUP ------------------------------- #
 # Window
 window = Tk()
@@ -29,14 +38,16 @@ lang_text = card_canvas.create_text(400, 150, text="Language", fill="black", fon
 word_text = card_canvas.create_text(400, 263, text="Word", fill="black", font=(FONT, 60, "bold"))
 card_canvas.grid(row=0, column=0, columnspan=2)
 
+generate_word()
+
 # Right Button
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=generate_word)
 right_button.grid(row=1, column=1)
 
 # Wrong Button
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=generate_word)
 wrong_button.grid(row=1, column=0)
 
 window.mainloop()
